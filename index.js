8be38a78c7d48637651c082b85f8733fe5ae690a
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-10-07 15:48:10 +0200

    another way doing rainbow

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
 function makeRainbows(string) {
 
   console.log("hello world");
-  console.log('colors'.green(string));
-  console.log('colors'.white(string));
-  console.log('colors'.yellow(string));
-  console.log('colors'.red(string));
-  console.log('colors'.blue(string));
-  console.log('colors'.green(string));
+  console.log(colors.green(string));
+  console.log(colors.white(string));
+  console.log(colors.yellow(string));
+  console.log(colors.red(string));
+  console.log(colors.blue(string));
+  console.log(colors.green(string));
 
 }
   for(var i = 2; i < process.argv.length; i++) {
