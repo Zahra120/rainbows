42ef72d4b605ab78633601a978614de0a6dab21f
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-10-07 15:41:35 +0200

    add process.argv

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-var colors = require('colors');
-console.log("hello world");
-console.log('hello'.green);
-console.log('hello world'.underline.red);
+
+
+// make the program automatically print the lines bellow every 1 secend
+  var colors = require('colors');
+
+function makeRainbows(string) {
+
+  console.log("hello world");
+  console.log('colors'.green(string));
+  console.log('colors'.white(string));
+  console.log('colors'.yellow(string));
+  console.log('colors'.red(string));
+  console.log('colors'.blue(string));
+  console.log('colors'.green(string));
+
+}
+  for(var i = 2; i < process.argv.length; i++) {
+    setInterval(makeRainbows, 5000,process.argv[i]);
+}
